15cb2778aa4381e086a73ab6c98ac26c01025b53
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-09-10 18:18:38 +0530

    applying smooth scrollinhg

--- script.js
+++ script.js
@@ -30,26 +30,26 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-/////
+///// 
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
 
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByClassName('btn'));
 
-// creating and inserting elements
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-// message.textContent = 'We use cookies to improve analytics.';
-message.innerHTML =
-  'We use cookies to improve analytics. <button class="btn btn--close-cookie">Got it!</button>';
-// header.prepend(message); // places as first child
-header.append(message); // places as last child
+// // creating and inserting elements
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// // message.textContent = 'We use cookies to improve analytics.';
+// message.innerHTML =
+//   'We use cookies to improve analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// // header.prepend(message); // places as first child
+// header.append(message); // places as last child
 
 // how to add at multiple places at once
 
@@ -59,46 +59,57 @@ header.append(message); // places as last child
 // header.after(message.cloneNode(true));
 
 // delete element
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    message.remove(); // Es6 method
-    // message.parentElement.removeChild(message); // pre ES6 method 
-  });
-  
+// document
+// .querySelector('.btn--close-cookie')
+// .addEventListener('click', function () {
+//   message.remove(); // Es6 method
+// message.parentElement.removeChild(message); // pre ES6 method
+// });
+
 // Style attributes and classes
 // styles
 
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
 
-console.log(getComputedStyle(message).height);
+// console.log(getComputedStyle(message).height);
 
 // css set property
 
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
 // document.body.style.setProperty('background-color', 'yellow'); // expirimented by myself
 
 // attributes
 
-const logo = document.querySelector('.nav__logo');
-console.log(logo);
-console.log(logo.getAttribute("designer"))// to get the non standarized attrb
-logo.setAttribute("Company", "Bankist") // use setAttribute to add attrb
-console.log(logo.getAttribute("src"));
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo);
+// console.log(logo.getAttribute("designer"))// to get the non standarized attrb
+// logo.setAttribute("Company", "Bankist") // use setAttribute to add attrb
+// console.log(logo.getAttribute("src"));
 
+// logo.alt = 'beautiful minimalist logo';
 
-logo.alt = 'beautiful minimalist logo';
+// const link = document.querySelector('.twitter-link');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
 
-const link = document.querySelector('.twitter-link');
-console.log(link.href);
-console.log(link.getAttribute('href'));
-
-// data attribute
-console.log(logo.dataset.versionNumber);
+// // data attribute
+// console.log(logo.dataset.versionNumber);
 
 // classes
 // logo.classList.add('c', 'j');
 // logo.classList.remove();
 // logo.classList.toggle();
 // logo.classList.contains();
+
+//////
+
+// smooth scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1Coords = section1.getBoundingClientRect();
+  console.log(s1Coords);
+});
