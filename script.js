6d0346992475a96791b2e69c7e12417d911e76e6
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-09-11 13:12:07 +0530

    learned events and practiced adding and removing them

--- script.js
+++ script.js
@@ -130,3 +130,17 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 
 });
+
+
+const h1 = document.querySelector('h1');
+
+const alertH1= function (e) {
+  alert('addEventListener: great! you are reading the heading :D');
+  h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: great! you are reading the heading :D');
+// };
