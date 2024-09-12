1f0d7421f077565927a4065c2c2cf5d497146403
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-09-12 23:25:09 +0530

    commented the code

--- script.js
+++ script.js
@@ -185,26 +185,27 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 // });
 //
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
-// going downwords : child
-console.log(h1.querySelectorAll('.highlight'));
-console.log(h1.childNodes);
-console.log(h1.children);
-// h1.firstElementChild.style.color = 'white';
-// h1.lastElementChild.style.color = 'orangered';
+// // going downwords : child
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// // h1.firstElementChild.style.color = 'white';
+// // h1.lastElementChild.style.color = 'orangered';
 
-// going upwards : parensts
-console.log(h1.parentNode);
-console.log(h1.parentElement);
-// h1.closest('.header').style.background = 'var(--gradient-secondary)';
-// h1.closest('h1').style.background = 'var(--gradient-primary)';
+// // going upwards : parensts
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+// // h1.closest('.header').style.background = 'var(--gradient-secondary)';
+// // h1.closest('h1').style.background = 'var(--gradient-primary)';
 
-// going sideways : siblings
-console.log(h1.previousElementSibling);
-console.log(h1.nextElementSibling);
+// // going sideways : siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
 
 // console.log(h1.parentElement.children);
 // [...h1.parentElement.children].forEach(function (el) {
 //   if (el !== h1) el.style.transform = 'scale(0.5)'
 // });
+
