98b25b17597d54ad8564fcbe782bcbc2b0c7688e
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-09-14 18:53:44 +0530

    applied complete functionality of handler nav

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ tabContainer.addEventListener('click', function (e) {
 });
 
 // menu fade animation
-const handleHover = function (e, opacity) {
+const handleHover = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
