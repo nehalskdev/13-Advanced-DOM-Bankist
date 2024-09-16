ab9f6bd984e5917f0fab0073405e147d120b6d91
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-09-16 12:50:10 +0530

    applied reveal section functionality

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
@@ -162,6 +162,29 @@ const headerObsever = new IntersectionObserver(stickyNav, {
 });
 
 headerObsever.observe(header);
+
+// reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 // const header = document.querySelector('.header');
 // const allSections = document.querySelectorAll('.section');
